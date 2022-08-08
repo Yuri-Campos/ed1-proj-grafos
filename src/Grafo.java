9b05b124c385824874cafd2e56dfbaf8b020ded6
Yuri Campos <yuri.g.s.campos@gmail.com>
2022-08-08 00:23:34 -0300

    adicionar contador de vertices e edges

--- src/Grafo.java
+++ src/Grafo.java
@@ -33,4 +33,23 @@ public class Grafo<T> {
 		}
 	}
 	
+	public void contaVertices()
+	{
+		System.out.println("O grafo possui: " + map.keySet().size() + " vertices");
+	}
+	
+	public void contaBordas(boolean bidirecional)
+	{
+		int contador = 0;
+		for(T v : map.keySet())
+		{
+			contador += map.get(v).size();
+		}
+		if(bidirecional == true)
+		{
+			contador = contador/2;
+		}
+		System.out.println("O grafo possui: " + contador + " bordas");
+	}
+	
 }
