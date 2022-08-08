618c2c181c69078e73ed33d30802d400953a4297
Yuri Campos <yuri.g.s.campos@gmail.com>
2022-08-08 00:55:29 -0300

    adicionar funcionalidade adicionar e ligar cidades

--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,7 @@ public class Main {
 		
 		int escolha;
 		do {
-			System.out.println("Escolha a opcao desejada: \n1-listar ligacoes\n2-verificar ligacao entre duas cidades\n0-sair");
+			System.out.println("Escolha a opcao desejada: \n1-listar ligacoes\n2-verificar ligacao entre duas cidades\n3-adicionar cidades\n0-sair");
 			escolha = scanner.nextInt();
 			switch(escolha)
 			{
@@ -27,6 +27,23 @@ public class Main {
 				System.out.println("Cidade 2: ");
 				String c2 = scanner.next();
 				caravanas.temLigacao(c1, c2);
+				break;
+			case 3:
+				int vF = -1;
+				System.out.println("Nome da cidade: ");
+				String cidadeAdd = scanner.next();
+				System.out.println("Tem conexao? (1-SIM 0-NAO)");
+				vF = scanner.nextInt();
+				if(vF == 1)
+				{
+					System.out.println("Cidade conectada: ");
+					String cidadeConect = scanner.next();
+					caravanas.addBorda(cidadeAdd, cidadeConect, true);
+					break;
+				}
+				else if(vF == 0) {	
+					caravanas.addVertice(cidadeAdd);
+				}
 				break;
 			case 0:
 				break;
